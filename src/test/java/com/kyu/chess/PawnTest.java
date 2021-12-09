d546e76df9baac96e7a398e5dd0dcedcd004d273
kyupid <iamscvv@gmail.com>
2021-12-09 16:41:19 +0900

    add: 검은색 폰 테스트

--- src/test/java/com/kyu/chess/PawnTest.java
+++ src/test/java/com/kyu/chess/PawnTest.java
@@ -8,9 +8,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어져야 한다")
+    @DisplayName("흰색 폰과 검은색 폰이 생성되어져야 한다")
     void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        Pawn blackPawn = new Pawn("black");
+
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
