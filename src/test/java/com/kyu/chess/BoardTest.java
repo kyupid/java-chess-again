f9ba33875976ff3b41f7172a623a3d9f5d99b2f9
kyupid <iamscvv@gmail.com>
2021-12-09 18:13:59 +0900

    add: `Board` 테스트
    - `Board`에 넣은 폰들이 잘 들어가졌는지 확인

---
+++ src/test/java/com/kyu/chess/BoardTest.java
@@ -0,0 +1,24 @@
+package com.kyu.chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class BoardTest {
+    @Test
+    @DisplayName("하얀색,검은색 폰을 생성해서 보드에 추가한뒤 잘들어간지 확인한다.")
+    void create() {
+        Board board = new Board();
+
+        Pawn whitePawn = new Pawn(PieceColor.WHITE.getColor());
+        board.add(whitePawn);
+        assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findPawn(0)).isEqualTo(whitePawn);
+
+        Pawn blackPawn = new Pawn(PieceColor.BLACK.getColor());
+        board.add(blackPawn);
+        assertThat(board.size()).isEqualTo(2);
+        assertThat(board.findPawn(1)).isEqualTo(blackPawn);
+    }
+}
