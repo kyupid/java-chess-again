aaa4a674f97f0a1cea24699ef019a8decbc2412e
kyupid <iamscvv@gmail.com>
2021-12-09 16:23:14 +0900

    add: 흰색 폰 생성 테스트

--- src/test/java/com/kyu/chess/PawnTest.java
+++ src/test/java/com/kyu/chess/PawnTest.java
@@ -1,4 +1,16 @@
 package com.kyu.chess;
 
-public class PawnTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어져야 한다")
+    void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
 }
