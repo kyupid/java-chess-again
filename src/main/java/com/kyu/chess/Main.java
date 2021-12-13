49ecff6cafb2bf62752789514bcbfb4e1b316e9b
kyupid <iamscvv@gmail.com>
2021-12-13 18:26:26 +0900

    add: 콘솔 입력을 통한 게임 시작&종료

--- src/main/java/com/kyu/chess/Main.java
+++ src/main/java/com/kyu/chess/Main.java
@@ -1,8 +1,17 @@
 package com.kyu.chess;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.initialize();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("start? y or n");
+        while (scanner.next().equals("y")) {
+            Board board = new Board();
+            board.initialize();
+            System.out.println("start? y or n");
+            // TODO: 체스말 액션 명령어
+        }
+        System.exit(1);
     }
 }
