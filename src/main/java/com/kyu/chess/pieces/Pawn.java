ad474fd04d2625de6d116aaac1cb7ead944acba2
kyupid <iamscvv@gmail.com>
2021-12-13 13:42:22 +0900

    [#3] add: `Pawn` 색상 REPRESENTATION 필드

--- src/main/java/com/kyu/chess/pieces/Pawn.java
+++ src/main/java/com/kyu/chess/pieces/Pawn.java
@@ -1,6 +1,9 @@
 package com.kyu.chess.pieces;
 
 public class Pawn {
+    private final char WHITE_REPRESENTATION = 'p';
+    private final char BLACK_REPRESENTATION = 'P';
+
     private String color;
 
     public Pawn() {
