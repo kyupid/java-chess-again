4a000d54e93b65266314b13ca79a667f434117c9
kyupid <iamscvv@gmail.com>
2021-12-14 00:15:58 +0900

    set: 두번째 파라미터 to null
    - 잘못넣었음

--- src/test/java/com/kyu/chess/pieces/PawnTest.java
+++ src/test/java/com/kyu/chess/pieces/PawnTest.java
@@ -12,7 +12,7 @@ class PawnTest {
     @DisplayName("Color와 Representation이 null인 기본생성자를 확인한다.")
     void createWithNoColorNoRepresentation() {
         Pawn pawn = new Pawn();
-        verify(pawn, pawn.getColor(), null);
+        verify(pawn, null, null);
     }
 
     @Test
