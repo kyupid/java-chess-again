4e6f6f0b8731a81d126a301e76ffca63066cb7c2
kyupid <iamscvv@gmail.com>
2021-12-13 21:31:49 +0900

    fix: 스캐너 받는 것을 좀 더 의미가 들어나도록 메소드로 분리

---
+++ src/main/java/com/kyu/chess/Game.java
@@ -0,0 +1,22 @@
+package com.kyu.chess;
+
+import java.util.Scanner;
+
+public class Game {
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        System.out.println("start? y or n");
+        while (selectedCommandToStartGame()) {
+            Board board = new Board();
+            board.initialize();
+            System.out.println("start? y or n");
+            // TODO: 체스말 액션
+        }
+        System.exit(1);
+    }
+
+    private boolean selectedCommandToStartGame() {
+        return scanner.next().equals("y");
+    }
+}
