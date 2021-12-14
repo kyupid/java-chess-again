356248d1c144bccc9603313736aeaa5b26667539
kyupid <iamscvv@gmail.com>
2021-12-14 17:48:12 +0900

    fix: `Board.initialize()` 메소드 분리
    - 메소드 단일책임원칙
    - 객체지향 생활체조
    - 2중 for문의 각각의 행동은 무엇?

--- src/main/java/com/kyu/chess/Board.java
+++ src/main/java/com/kyu/chess/Board.java
@@ -3,26 +3,11 @@ package com.kyu.chess;
 import com.kyu.chess.pieces.Pawn;
 
 public class Board {
-    private final String blank = ".";
+
     private String[][] map = new String[8][8];
 
     public void initialize() {
-        final int WHITE_PAWNS_LINE_NUMBER = 1;
-        final int BLACK_PAWNS_LINE_NUMBER = 6;
-
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                map[i][j] = blank;
-                if (i == WHITE_PAWNS_LINE_NUMBER) {
-                    map[i][j] = new Pawn(PieceColor.WHITE.getColor(), Pawn.WHITE_REPRESENTATION).getRepresentation();
-                }
-                if (i == BLACK_PAWNS_LINE_NUMBER) {
-                    map[i][j] = new Pawn(PieceColor.BLACK.getColor(), Pawn.BLACK_REPRESENTATION).getRepresentation();
-                }
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
+        divideRow();
     }
 
     public void printCurrentBoard() {
@@ -31,6 +16,30 @@ public class Board {
                 System.out.print(str);
             }
             System.out.println();
+        }
+    }
+
+    private void divideRow() {
+        for (int i = 0; i < map.length; i++) {
+            placePawns(i);
+            System.out.println();
+        }
+    }
+
+    private void placePawns(int i) {
+        final int WHITE_PAWNS_LINE_NUMBER = 1;
+        final int BLACK_PAWNS_LINE_NUMBER = 6;
+        final String BLANK = ".";
+
+        for (int j = 0; j < map.length; j++) {
+            map[i][j] = BLANK;
+            if (i == WHITE_PAWNS_LINE_NUMBER) {
+                map[i][j] = new Pawn(PieceColor.WHITE.getColor(), Pawn.WHITE_REPRESENTATION).getRepresentation();
+            }
+            if (i == BLACK_PAWNS_LINE_NUMBER) {
+                map[i][j] = new Pawn(PieceColor.BLACK.getColor(), Pawn.BLACK_REPRESENTATION).getRepresentation();
+            }
+            System.out.print(map[i][j]);
         }
     }
 
