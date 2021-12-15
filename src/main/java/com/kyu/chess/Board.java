e41159817e40fbc232331d9f0a761b8d40937db4
kyupid <iamscvv@gmail.com>
2021-12-15 16:13:09 +0900

    add: `Board` 폰 이외에 나머지 체스말 올리기

--- src/main/java/com/kyu/chess/Board.java
+++ src/main/java/com/kyu/chess/Board.java
@@ -28,12 +28,17 @@ public class Board {
     }
 
     private void placePawns(int i) {
+        final int WHITE_PIECES_LINE_NUMBER = 0;
         final int WHITE_PAWNS_LINE_NUMBER = 1;
         final int BLACK_PAWNS_LINE_NUMBER = 6;
+        final int BLACK_PIECES_LINE_NUMBER = 7;
         final String BLANK = ".";
 
         for (int j = 0; j < map.length; j++) {
             map[i][j] = BLANK;
+            if (i == WHITE_PIECES_LINE_NUMBER) {
+                map[i][j] = placeWhitePieces(j);
+            }
             if (i == WHITE_PAWNS_LINE_NUMBER) {
                 map[i][j] = new Piece(PieceStatus.WHITE_PAWN)
                         .getPieceStatus()
@@ -44,8 +49,53 @@ public class Board {
                         .getPieceStatus()
                         .getRepresentation();
             }
+            if (i == BLACK_PIECES_LINE_NUMBER) {
+                map[i][j] = placeBlackPieces(j);
+            }
             System.out.print(map[i][j]);
         }
+    }
+
+    private String placeWhitePieces(int j) {
+        switch (j) {
+            case 0:
+            case 7:
+                return new Piece(PieceStatus.WHITE_ROOK).getPieceStatus().getRepresentation();
+            case 1:
+            case 6:
+                return new Piece(PieceStatus.WHITE_KNIGHT).getPieceStatus().getRepresentation();
+            case 2:
+            case 5:
+                return new Piece(PieceStatus.WHITE_BISHOP).getPieceStatus().getRepresentation();
+            case 3:
+                return new Piece(PieceStatus.WHITE_QUEEN).getPieceStatus().getRepresentation();
+            case 4:
+                return new Piece(PieceStatus.WHITE_KING).getPieceStatus().getRepresentation();
+            default:
+                break;
+        }
+        return new Piece().getPieceStatus().getRepresentation();
+    }
+
+    private String placeBlackPieces(int j) {
+        switch (j) {
+            case 0:
+            case 7:
+                return new Piece(PieceStatus.BLACK_ROOK).getPieceStatus().getRepresentation();
+            case 1:
+            case 6:
+                return new Piece(PieceStatus.BLACK_KNIGHT).getPieceStatus().getRepresentation();
+            case 2:
+            case 5:
+                return new Piece(PieceStatus.BLACK_BISHOP).getPieceStatus().getRepresentation();
+            case 3:
+                return new Piece(PieceStatus.BLACK_QUEEN).getPieceStatus().getRepresentation();
+            case 4:
+                return new Piece(PieceStatus.BLACK_KING).getPieceStatus().getRepresentation();
+            default:
+                break;
+        }
+        return new Piece().getPieceStatus().getRepresentation();
     }
 
     public String[][] getMap() {
